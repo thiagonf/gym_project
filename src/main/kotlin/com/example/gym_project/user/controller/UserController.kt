ddbabfadba76fbc6a8130acfcd4a577d138a7246
Thiago Nogueira <thiagonf10@gmail.com>
2021-01-29 12:06:19 -0300

    #2 Adding controller list users

--- src/main/kotlin/com/example/gym_project/user/controller/UserController.kt
+++ src/main/kotlin/com/example/gym_project/user/controller/UserController.kt
@@ -4,7 +4,22 @@ import com.example.gym_project.user.model.User
 
 class UserController {
     var listUser: MutableList<User> =  mutableListOf<User>()
-    fun create(user : User){
+    fun create(user : User): User{
         listUser.add(user)
+        return user
+    }
+
+    fun listById(id: Short): List<User> {
+        val filterUsers: List<User> = listUser.filter { user -> user.id == id }
+        if(filterUsers.isNotEmpty()){
+            return filterUsers
+        } else{
+            throw NoSuchElementException("User id $id not found!")
+        }
+
+    }
+
+    fun listAll(): List<User> {
+        return listUser
     }
 }
