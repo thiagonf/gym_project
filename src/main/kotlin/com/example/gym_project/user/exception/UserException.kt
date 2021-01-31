c60e5d4112e51e9268d4320d304fbb2f644835e6
Thiago Nogueira <thiagonf10@gmail.com>
2021-01-31 13:52:04 -0300

    #3 Adding PSQLException in UserException

--- src/main/kotlin/com/example/gym_project/user/exception/UserException.kt
+++ src/main/kotlin/com/example/gym_project/user/exception/UserException.kt
@@ -1,6 +1,7 @@
-package com.example.gym_project.user.controller
+package com.example.gym_project.user.exception
 
 import com.example.gym_project.exception.ErrorResponse
+import org.postgresql.util.PSQLException
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ControllerAdvice
@@ -18,5 +19,10 @@ class UserException {
     @ExceptionHandler(NoSuchElementException::class)
     fun noSuchElementException(exception: NoSuchElementException): ResponseEntity<ErrorResponse>  {
         return errorResponse.generateErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR, exception.message.toString(), exception)
+    }
+
+    @ExceptionHandler(PSQLException::class)
+    fun postgresException(exception: PSQLException): ResponseEntity<ErrorResponse> {
+        return errorResponse.generateErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR, exception.localizedMessage.substringAfter("Detail"), exception)
     }
 }
