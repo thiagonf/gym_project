a532970cfea18a71cf673603f30e27d53c740739
Thiago Nogueira <thiagonf10@gmail.com>
2021-01-31 13:48:50 -0300

    #3 Fixing User model to create entity in database postgres

--- src/main/kotlin/com/example/gym_project/user/model/User.kt
+++ src/main/kotlin/com/example/gym_project/user/model/User.kt
@@ -1,13 +1,25 @@
 package com.example.gym_project.user.model
 
+import javax.persistence.*
+
+@Entity
+@Table(name = "users")
 data class User(
-    val id: Short,
-    var name: String,
-    var email: String,
-    var phone: String){
-    init {
-        if (email.isEmpty() || !email.matches(Regex("^[a-zA-Z0-9]+@[a-zA-Z0-9]+(.[a-zA-Z]{2,})$"))) {
-            throw IllegalArgumentException("$email is not a valid email address")
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    var id: Int=0,
+    var name: String="",
+    @Column(unique = true, nullable = false)
+    var email: String="",
+    var phone: String=""){
+
+    internal fun validate(user: User) {
+        this.validateEmail(user)
+    }
+
+    private fun validateEmail(user: User){
+        if (user.email.isEmpty() || !user.email.matches(Regex("^[a-zA-Z0-9]+@[a-zA-Z0-9]+(.[a-zA-Z]{2,})$"))) {
+            throw IllegalArgumentException("${user.email} is not a valid email address")
         }
     }
 }
