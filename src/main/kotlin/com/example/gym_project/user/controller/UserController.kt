2d223f8c5ad5185e6c9c1be54a05b405f4f5c654
Thiago Nogueira <thiagonf10@gmail.com>
2021-01-31 13:51:06 -0300

    #3 Fixing controller User use repository

--- src/main/kotlin/com/example/gym_project/user/controller/UserController.kt
+++ src/main/kotlin/com/example/gym_project/user/controller/UserController.kt
@@ -1,25 +1,32 @@
 package com.example.gym_project.user.controller
 
 import com.example.gym_project.user.model.User
+import com.example.gym_project.user.repo.UserRepository
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.*
 
-class UserController {
-    var listUser: MutableList<User> =  mutableListOf<User>()
-    fun create(user : User): User{
-        listUser.add(user)
-        return user
+@RestController
+class UserController(){
+    @Autowired
+    private lateinit var repository: UserRepository
+
+    @PostMapping("/users")
+    @ResponseStatus(HttpStatus.CREATED)
+    fun create(@RequestBody user: User): User {
+        user.validate(user)
+        return repository.save(user)
     }
 
-    fun listById(id: Short): List<User> {
-        val filterUsers: List<User> = listUser.filter { user -> user.id == id }
-        if(filterUsers.isNotEmpty()){
-            return filterUsers
-        } else{
-            throw NoSuchElementException("User id $id not found!")
-        }
-
+    @GetMapping("/users")
+    @ResponseStatus(HttpStatus.OK)
+    fun listUser(): MutableIterable<User> {
+        return repository.findAll()
     }
 
-    fun listAll(): List<User> {
-        return listUser
+    @GetMapping("/users/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    fun listUserById(@PathVariable id: Int): Iterable<User> {
+        return repository.findById(id)
     }
 }
